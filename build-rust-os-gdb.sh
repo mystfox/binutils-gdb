07fa1179bee08c88cc2c6fc364411a738e2a0a04
Philipp Oppermann <dev@phil-opp.com>
2015-12-18 12:35:05 +0100

    Add script to build the custom GDB easily
    It gets built in a `rust-os-gdb` subfolder. The following dependencies are required:
    
    `apt-get install texinfo flex bison python-dev`

---
+++ build-rust-os-gdb.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Exit if anything fails
+set -e
+
+DIR=$( pwd )
+
+git clone -b rust-os --depth 1 \
+  https://github.com/phil-opp/binutils-gdb.git
+mkdir build-binutils
+cd build-binutils
+../binutils-gdb/configure --target=x86_64-pc-linux-gnu --prefix="$DIR/rust-os-gdb" --with-python=yes \
+  --disable-nls --disable-werror \
+  --disable-gas --disable-binutils --disable-ld --disable-gprof
+make
+make install
+cd ..
+rm -rf build-binutils
+rm -rf binutils-gdb
